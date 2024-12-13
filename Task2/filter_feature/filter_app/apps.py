38386d7ff52b812c90f0624cc5ab0c792e50e040
Vishal5725 <141394556+Vishal5725@users.noreply.github.com>
2024-12-13 21:46:37 +0530

    Delete Task2/filter_feature directory

--- Task2/filter_feature/filter_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FilterAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'filter_app'
--- Task2/filter_feature/filter_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Box(models.Model):
-    category = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.category} - {self.location}"
